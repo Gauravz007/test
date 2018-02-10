c444079362b348a293d58883b6416c0f07e9a1f8
Gauravz007 <gaurav1564@gmail.com>
2018-02-10 14:20:59 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,9 @@
 custcodelink = {
 ':wait':'4/smile_wait.gif',
-':killinsect':'5/fgsf.gif'
+':killinsect':'5/fgsf.gif',
+':shoot':'2/dfhfdz.gif',
+':killinsect':'5/fgsf.gif',
+':killinsect':'5/fgsf.gif',
+':killinsect':'5/fgsf.gif',
+':killinsect':'5/fgsf.gif',
 };
