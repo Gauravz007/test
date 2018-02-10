2f01f4e00b9f79de18a87b9ba5327ed308b393d7
Gauravz007 <gaurav1564@gmail.com>
2018-02-10 18:38:14 +0530

    Update test.js

--- test.js
+++ test.js
@@ -16,4 +16,15 @@ custcodelink = {
 ':bore:':'4/boredom.gif',
 ':giverose:':'2/giveroseplz.gif',
 ':fire:':'2/sfgsg.gif' 
+':party3:':'66740/1.gif',
+':notme2:':'46745/2.gif',
+':alright2:':'41060/3.gif',
+':notme3:':'37495/4.gif',
+':tomato:':'34157/5.gif',
+':girlmad:':'30828/6.gif',
+':girlclaps:':'28394/7.gif',
+':king:':'26361/8.gif',
+':laugh2:':'24040/9.gif',
+':cry3:':'26054/10.gif',
+':write:':'25062/11.gif'
 };
