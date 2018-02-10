b7b2d320c01775ad1dcdd87abc7c4ead5714b65c
Gauravz007 <gaurav1564@gmail.com>
2018-02-10 14:26:46 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 custcodelink = {
-':wait':'4/smile_wait.gif',
-':killinsect':'5/fgsf.gif',
-':shoot':'2/dfhfdz.gif',
-':killinsect':'5/fgsf.gif',
-':killinsect':'5/fgsf.gif',
-':killinsect':'5/fgsf.gif',
-':killinsect':'5/fgsf.gif',
+':wait:':'4/smile_wait.gif',
+':killinsect:':'5/fgsf.gif',
+':shoot:':'2/dfhfdz.gif',
+':cray:':'3/cray2.png',
+':danks:':'6/thank_you.gif',
+':budhau:':'2/59b05971add3f.gif',
+':good:':'https://pzy.be/i/6/good2.gif'
 };
