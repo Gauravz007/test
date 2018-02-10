00f0cf76288491faca09fc8fad33900148dec002
Gauravz007 <gaurav1564@gmail.com>
2018-02-10 14:57:34 +0530

    Update test.js

--- test.js
+++ test.js
@@ -5,5 +5,10 @@ custcodelink = {
 ':cray:':'3/cray2.png',
 ':danks:':'6/thank_you.gif',
 ':budhau:':'2/59b05971add3f.gif',
-':good:':'6/good2.gif'
+':good:':'6/good2.gif',
+':yoga:':'2/meditation.gif',
+':hack:':'2/hacker.gif',
+':thatsgay:':'3/Gay.png',
+':dacin:':'2/dancin.gif',
+':timeout:':'1/timeout.gif'
 };
